365f26a5cae22fb4580b11d540f5832ecc07f6ef
Carlos <adiz98@hotmail.com>
2018-10-30 19:11:30 -0500

    commit

---
+++ AdminFree/src/app/shell/bar-user/bar-user.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'admin-bar-user',
+  templateUrl: './bar-user.component.html',
+  styleUrls: ['./bar-user.component.css']
+})
+export class BarUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- AdminFree/src/app/shell/shell.module.ts
+++ AdminFree/src/app/shell/shell.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { BodyComponent } from './body/body.component';
 import { ContentComponent } from './content/content.component';
 import { TitleBarComponent } from './title-bar/title-bar.component';
+import { BarUserComponent } from './bar-user/bar-user.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [BodyComponent, ContentComponent, TitleBarComponent],
+  declarations: [BodyComponent, ContentComponent, TitleBarComponent, BarUserComponent],
   exports: [BodyComponent]
 })
 export class ShellModule { }
