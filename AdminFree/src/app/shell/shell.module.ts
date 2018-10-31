22d96eca58cc2735d1093f3c0192a23db93ce7ea
Carlos <adiz98@hotmail.com>
2018-10-30 22:03:44 -0500

    commit

--- AdminFree/src/app/shell/shell.module.ts
+++ AdminFree/src/app/shell/shell.module.ts
@@ -4,12 +4,13 @@ import { BodyComponent } from './body/body.component';
 import { ContentComponent } from './content/content.component';
 import { TitleBarComponent } from './title-bar/title-bar.component';
 import { BarUserComponent } from './bar-user/bar-user.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [BodyComponent, ContentComponent, TitleBarComponent, BarUserComponent],
+  declarations: [BodyComponent, ContentComponent, TitleBarComponent, BarUserComponent, FooterComponent],
   exports: [BodyComponent]
 })
 export class ShellModule { }
--- AdminFree/src/app/shell/title-bar/title-bar.component.ts
+++ AdminFree/src/app/shell/title-bar/title-bar.component.ts
@@ -1,3 +1,4 @@
+import { ScreenService } from './../services/screen.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TitleBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public screenService: ScreenService) { }
 
   ngOnInit() {
   }
