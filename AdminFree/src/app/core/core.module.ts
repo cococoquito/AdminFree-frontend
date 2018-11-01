d41f7cc5b3b53e80d8cf3ea93d6c60a6b1b0e9da
Carlos <adiz98@hotmail.com>
2018-11-01 18:22:50 -0500

    commit

--- AdminFree/src/app/core/core.module.ts
+++ AdminFree/src/app/core/core.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpRequestInterceptor } from './../security/http-request.interceptor';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 /**
  * Modulo Core de la aplicacion, aca se debe agregar todos los modulos,
