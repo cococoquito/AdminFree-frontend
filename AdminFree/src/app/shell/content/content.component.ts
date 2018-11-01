7afdb8301881fd7a4e2e5800bc105d8d939d3fdc
Carlos <adiz98@hotmail.com>
2018-11-01 10:25:46 -0500

    commit

--- AdminFree/src/app/shell/content/content.component.ts
+++ AdminFree/src/app/shell/content/content.component.ts
@@ -1,11 +1,26 @@
 import { ScreenService } from './../services/screen.service';
 import { MenuService } from './../services/menu.service';
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'admin-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.css'],
+  animations: [
+
+    trigger('openClose', [
+      state('open', style({ width: '225px' })),
+      state('closed', style({ width: '0px' })),
+      transition('* => *', animate(300))
+    ]),
+  ]
 })
 export class ContentComponent implements OnInit {
 
