eea5caa4b881a8ad3266c42949020d2bfd299d71
Carlos <adiz98@hotmail.com>
2018-11-02 08:40:44 -0500

    commit

--- AdminFree/src/app/shell/content/content.component.ts
+++ AdminFree/src/app/shell/content/content.component.ts
@@ -1,31 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ShellService } from './../../core/services/shell.service';
 import {
   trigger,
   state,
   style,
   animate,
-  transition,
+  transition
 } from '@angular/animations';
 
+/**
+ * Componente que contiene el router para incluir los demas componentes
+ * dependiendo de la navegacion, este componente tambien contiene los menus
+ * para dispositivos con resolucion grande y pequenia
+ *
+ * @author Carlos Andres Diaz
+ */
 @Component({
   selector: 'admin-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.css'],
   animations: [
-
     trigger('openClose', [
       state('open', style({ width: '225px' })),
       state('closed', style({ width: '0px' })),
       transition('* => *', animate(300))
-    ]),
+    ])
   ]
 })
-export class ContentComponent implements OnInit {
-
-  constructor(public shell: ShellService) { }
-
-  ngOnInit() {
-  }
-
+export class ContentComponent {
+  constructor(public shell: ShellService) {}
 }
