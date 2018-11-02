a8338a54b67731d3b28670944cd82a1886eb4dd2
Carlos <adiz98@hotmail.com>
2018-11-02 07:38:49 -0500

    commit

--- AdminFree/src/app/shell/body/body.component.ts
+++ AdminFree/src/app/shell/body/body.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+/**
+ * Es el body del shell de la aplicacion, contiene toda la estructura tales como
+ * header, content, footer, el modulo shell exporta unicamente este componente
+ * para que los demas componentes lo pueda incluir en sus modulos
+ *
+ * @author Carlos Andres Diaz
+ */
 @Component({
   selector: 'admin-body',
   templateUrl: './body.component.html',
   styleUrls: ['./body.component.css']
 })
-export class BodyComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class BodyComponent {}
--- AdminFree/src/app/shell/footer/footer.component.ts
+++ AdminFree/src/app/shell/footer/footer.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
+/**
+ * Componente que representa el footer del Shell de la aplicacion
+ *
+ * @author Carlos Andres Diaz
+ */
 @Component({
   selector: 'admin-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })
-export class FooterComponent implements OnInit {
-
-  constructor() { }
-  ngOnInit() {
-  }
-}
+export class FooterComponent {}
