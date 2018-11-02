81e4ebef83658edcd8a8f097a1a6b3b12e6f0d5a
Carlos <adiz98@hotmail.com>
2018-11-02 10:03:10 -0500

    commit

--- AdminFree/src/app/shell/menus/menu-item/menu-item.component.ts
+++ AdminFree/src/app/shell/menus/menu-item/menu-item.component.ts
@@ -1,16 +1,17 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MenuItem } from './../../../model/common/menu-item';
 
+/**
+ * Es el Item del menu a visualizar en el Shell
+ *
+ * @author Carlos Andres Diaz
+ */
 @Component({
   selector: 'admin-menu-item',
   templateUrl: './menu-item.component.html',
   styleUrls: ['./menu-item.component.css']
 })
-export class MenuItemComponent implements OnInit {
-  @Input() item = <MenuItem>null; // MenuItem; <-- see angular-cli issue #2034
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class MenuItemComponent {
+  @Input()
+  public item = <MenuItem>null;
 }
--- AdminFree/src/app/shell/menus/menu/menu.component.ts
+++ AdminFree/src/app/shell/menus/menu/menu.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ShellService } from '../../../core/services/shell.service';
 
+/**
+ * Es el Menu del shell a visualizar en la aplicacion
+ *
+ * @author Carlos Andres Diaz
+ */
 @Component({
   selector: 'admin-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.css']
 })
-export class MenuComponent implements OnInit {
-
-  constructor(public shell: ShellService) { }
-
-  ngOnInit() {
-  }
-
+export class MenuComponent {
+  constructor(public shell: ShellService) {}
 }
