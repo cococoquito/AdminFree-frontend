f937be291ba4bda2c837e17199479c12c32c2bd0
Carlos <adiz98@hotmail.com>
2018-11-03 19:11:28 -0500

    commit

--- AdminFree/src/app/modules/shared/shared.module.ts
+++ AdminFree/src/app/modules/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { NoEspaciosBlancoDirective } from './../../directives/validators/espacios-blanco/no-espacios-blanco.directive';
+import { AutofocusDirective } from './../../directives/focus/autofocus.directive';
 
 /**
  * Modulo que contiene los modulos, componentes comunes
@@ -11,7 +12,12 @@ import { NoEspaciosBlancoDirective } from './../../directives/validators/espacio
  */
 @NgModule({
   imports: [CommonModule, FormsModule],
-  exports: [CommonModule, FormsModule, NoEspaciosBlancoDirective],
-  declarations: [NoEspaciosBlancoDirective]
+  exports: [
+    CommonModule,
+    FormsModule,
+    NoEspaciosBlancoDirective,
+    AutofocusDirective
+  ],
+  declarations: [NoEspaciosBlancoDirective, AutofocusDirective]
 })
 export class SharedModule {}
