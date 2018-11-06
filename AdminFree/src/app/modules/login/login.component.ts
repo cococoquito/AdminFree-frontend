14668fdadb059b7bdb97f1ebfe3cbc70231181e6
Carlos <adiz98@hotmail.com>
2018-11-06 18:56:07 -0500

    commit

--- AdminFree/src/app/modules/login/login.component.ts
+++ AdminFree/src/app/modules/login/login.component.ts
@@ -1,11 +1,11 @@
-import { TipoEventoConstant } from './../../constants/tipo-evento.constant';
-import { LocalStoreState } from './../../states/local-store.state';
 import { Component, OnInit } from '@angular/core';
 import { CommonComponent } from './../../util-class/common.component';
 import { SeguridadService } from './../../services/seguridad.service';
+import { LocalStoreState } from './../../states/local-store.state';
 import { CredencialesDTO } from './../../dtos/seguridad/credenciales.dto';
 import { UsuarioDTO } from './../../dtos/seguridad/usuario.dto';
 import { ClienteDTO } from './../../dtos/configuraciones/cliente.dto';
+import { TipoEventoConstant } from './../../constants/tipo-evento.constant';
 
 /**
  * Componente para la autenticacion del sistema ADMINFREE
