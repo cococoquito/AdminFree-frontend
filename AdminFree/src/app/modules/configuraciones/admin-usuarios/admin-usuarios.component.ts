93ddcc37a4c1ff7c4668c6fdebe701bff9b2453b
Carlos <adiz98@hotmail.com>
2018-11-27 07:44:35 -0500

    commit

--- AdminFree/src/app/modules/configuraciones/admin-usuarios/admin-usuarios.component.ts
+++ AdminFree/src/app/modules/configuraciones/admin-usuarios/admin-usuarios.component.ts
@@ -3,6 +3,7 @@ import { LocalStoreUtil } from './../../../util/local-store.util';
 import { UsuarioDTO } from './../../../dtos/seguridad/usuario.dto';
 import { ClienteDTO } from './../../../dtos/configuraciones/cliente.dto';
 import { AdminUsuarioService } from './../../../services/admin-usuario.service';
+import { OverlayPanel } from 'primeng/overlaypanel';
 
 /**
  * Componente para la administracion de los Usuarios del sistema
@@ -16,6 +17,8 @@ import { AdminUsuarioService } from './../../../services/admin-usuario.service';
 })
 export class AdminUsuariosComponent implements OnInit {
   public usuarios: Array<UsuarioDTO>;
+  public selectedCar1;
+  public nuevoUsuario: UsuarioDTO;
 
   private cliente: ClienteDTO;
 
@@ -33,5 +36,13 @@ export class AdminUsuariosComponent implements OnInit {
       },
       error => {}
     );
+  }
+
+  public showPanelCrearUsuario(): void {
+    this.nuevoUsuario = new UsuarioDTO();
+  }
+
+  public closePanelCrearUsuario(): void {
+    this.nuevoUsuario = null;
   }
 }
