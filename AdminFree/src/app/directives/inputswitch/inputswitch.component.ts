e55693309218fc9f73402220c6be2a11e7a60e85
Carlos <adiz98@hotmail.com>
2018-12-15 11:10:03 -0500

    agrega input-switch

---
+++ AdminFree/src/app/directives/inputswitch/inputswitch.component.ts
@@ -0,0 +1,29 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+/**
+ * Componente que es usuado para seleccionar un valor boolean
+ */
+@Component({
+  selector: 'admin-inputswitch',
+  templateUrl: './inputswitch.component.html',
+  styleUrls: ['./inputswitch.component.css']
+})
+export class InputswitchComponent {
+
+  /** bandera que indica el valor seleccionado por el usuario */
+  @Input() public isChecked: boolean;
+
+  /** Se utiliza para notificar el cambio del input */
+  @Output() public switchChange = new EventEmitter<boolean>();
+
+  /**
+   * Metodo que soporta el evento click del input-switch
+   */
+  public change() {
+    // se cambia la bandera
+    this.isChecked = !this.isChecked;
+
+    // se notifica el cambio
+    this.switchChange.emit(this.isChecked);
+  }
+}
