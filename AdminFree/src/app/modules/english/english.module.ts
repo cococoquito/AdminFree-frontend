b1bbceae331fec35b9716fbbdb4b442c6c6aec58
Your Name <you@example.com>
2019-12-01 09:55:56 -0500

    ajustes

--- AdminFree/src/app/modules/english/english.module.ts
+++ AdminFree/src/app/modules/english/english.module.ts
@@ -1,9 +1,14 @@
 import { NgModule } from '@angular/core';
+import { ConfirmationService, MessageService } from 'primeng/api';
 import { RouterModule } from '@angular/router';
 import { SeriesComponent } from './series/series.component';
 import { CreateSeriesComponent } from './create-series/create-series.component';
 import { RouterConstant } from './../../constants/router.constant';
 import { SharedModule } from '../shared/shared.module';
+import { EnglishService } from 'src/app/services/english.service';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ToastModule } from 'primeng/toast';
+import { MessagesModule } from 'primeng/messages';
 
 /**
  * Modulo que contiene los componentes y servicios para la
@@ -23,11 +28,19 @@ import { SharedModule } from '../shared/shared.module';
         component: CreateSeriesComponent
       }
     ]),
-    SharedModule
+    SharedModule,
+    MessagesModule,
+    ConfirmDialogModule,
+    ToastModule
   ],
   declarations: [
     SeriesComponent,
     CreateSeriesComponent
+  ],
+  providers: [
+    ConfirmationService,
+    MessageService,
+    EnglishService
   ]
 })
 export class EnglishModule {}
