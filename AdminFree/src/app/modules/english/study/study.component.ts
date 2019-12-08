52c823c7aadc92d4f2f8f7b1d80e013a60cac0b7
Your Name <you@example.com>
2019-12-08 10:28:22 -0500

    ajustes

--- AdminFree/src/app/modules/english/study/study.component.ts
+++ AdminFree/src/app/modules/english/study/study.component.ts
@@ -4,6 +4,13 @@ import { MessageService, ConfirmationService } from 'primeng/api';
 import { MsjFrontConstant } from 'src/app/constants/messages-frontend.constant';
 import { RouterConstant } from 'src/app/constants/router.constant';
 import { Router } from '@angular/router';
+import { EnglishService } from 'src/app/services/english.service';
+import { SerieDTO } from 'src/app/dtos/english/serie.dto';
+import { LocalStoreUtil } from 'src/app/util/local-store.util';
+import { TipoEventoConstant } from 'src/app/constants/tipo-evento.constant';
+import { MsjUtil } from 'src/app/util/messages.util';
+import { SeasonDTO } from 'src/app/dtos/english/season.dto';
+import { ChapterDTO } from 'src/app/dtos/english/chapter.dto';
 
 /**
  * Componente para la pagina de estudio
@@ -17,6 +24,15 @@ import { Router } from '@angular/router';
 })
 export class StudyComponent extends CommonComponent implements OnInit {
 
+  /** Es la serie seleccionada para estudiar*/
+  public serie: SerieDTO;
+
+  /** Es el capitulo a visualizar su detalle*/
+  public chapterDetail: ChapterDTO;
+
+  /** Es la temporada seleccionada para agregar o editar el capitulo*/
+  public seasonSelected: SeasonDTO;
+
   /**
    * @param messageService, Se utiliza para la visualizacion
    * de los mensajes en la pantalla
@@ -25,11 +41,15 @@ export class StudyComponent extends CommonComponent implements OnInit {
    * 
    * @param confirmationService, se utiliza para mostrar el
    * modal de confirmacion para diferente procesos
+   *
+   * @param englishService, se utiliza para consumir
+   * los servicios relacionados a este proceso negocio
    */
   constructor(
     protected messageService: MessageService,
     private router: Router,
-    private confirmationService: ConfirmationService) {
+    private confirmationService: ConfirmationService,
+    private englishService: EnglishService) {
     super();
   }
 
@@ -37,6 +57,7 @@ export class StudyComponent extends CommonComponent implements OnInit {
    * Se debe inicializar las variables globales
    */
   ngOnInit() {
+    this.init();
   }
 
   /**
@@ -50,5 +71,71 @@ export class StudyComponent extends CommonComponent implements OnInit {
         this.router.navigate([RouterConstant.ROUTER_ENGLISH]);
       }
     });
+  }
+
+  /**
+   * Metodo que permite consultar el detalle del capitulo
+   */
+  public clickChapter(idChapter: number, seasonSelected: SeasonDTO): void {
+    if (this.chapterDetail) {
+      this.confirmationService.confirm({
+        message: MsjFrontConstant.SEGURO_SALIR,
+        header: MsjFrontConstant.CONFIRMACION,
+        accept: () => {
+          this.getDetailChapter(idChapter, seasonSelected);
+        }
+      });
+    } else {
+      this.getDetailChapter(idChapter, seasonSelected);
+    }
+  }
+
+  /**
+   * Metodo que permite consultar el detalle del capitulo
+   */
+  private getDetailChapter(idChapter: number, seasonSelected: SeasonDTO): void {
+
+    // se limpia las variables globales
+    this.seasonSelected = null;
+    this.chapterDetail = null;
+
+    // se configura la temporada seleccionada
+    this.seasonSelected = seasonSelected;
+
+    // se procede a consultar el detalle del capitulo
+    this.englishService.getDetailChapter(idChapter).subscribe(
+      data => {
+        this.chapterDetail = data;
+      },
+      error => {
+        this.messageService.add(MsjUtil.getMsjError(this.showMensajeError(error)));
+      }
+    );
+  }
+
+  /**
+   * Se debe consultar los detalles de la serie
+   */
+  private init(): void {
+
+    // se obtiene el identificador de la serie del localstore
+    const idSerie = LocalStoreUtil.idSerie(TipoEventoConstant.GET);
+
+    // solo aplica si hay id de la serie en el local
+    if (idSerie) {
+
+      // se debe eliminar el id del local
+      LocalStoreUtil.idSerie(TipoEventoConstant.ELIMINAR);
+
+      // se procede a buscar el detalle de la serie a editar
+      this.englishService.getDetailSerie(idSerie).subscribe(
+        data => {
+          this.serie = data;
+        },
+        error => {
+          this.messageService.add(MsjUtil.getMsjError(this.showMensajeError(error)));
+        }
+      );
+    }
   }
 }
